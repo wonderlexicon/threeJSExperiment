957835a0c7a6ab73a0489b54d5437385ae59b1fa
wonderlexicon <nh756@nyu.edu>
2020-11-26 11:38:52 -0500

    created baseline code for new three.js project

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log 
+("whatev");
+import * as THREE from './resources/threejs/r122/build/three.module.js';
+
+
+
+
+function main(){
+    let canvas = document.querySelector("#canvas");
+    let renderer = new THREE.WebGLRenderer({canvas});  
+    
+}
