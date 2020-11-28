2d9d921b2537e730ea622076cc748a67b58c35cf
wonderlexicon <nh756@nyu.edu>
2020-11-28 11:33:01 -0500

    added phong material, added directional lighting, added cube rotation and animation

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function main(){
     let canvas = document.querySelector("#canvas");
     let renderer = new THREE.WebGLRenderer({canvas});  
     //the number of angles in between left and right
-    let fieldOfVisionDegrees = 150;
+    let fieldOfVisionDegrees = 75;
     //the ratio of ratio of width to height
-    let aspectRatio = 2;
+    let aspectRatio = 2.3;
     //degree of perceived close up perspective (ND >FD see more dimension of depth)
     let nearDistance = 0.1;
-    let farDistance = 5;
+    let farDistance = 3.5;
     let camera = new THREE.PerspectiveCamera(
         fieldOfVisionDegrees,
         aspectRatio,
@@ -22,11 +22,22 @@ function main(){
         farDistance,
     );
     //camera placement z moves front to back creating perceived depth - distance btwn viewer and object 
-    camera.position.z = 1.5;
-    camera.position.x = 2;
-    camera.position.y = 2;
+    camera.position.z = 2.1;
+    camera.position.x = 1.5;
+    camera.position.y = .01;
 
     let scene = new THREE.Scene();
+    {
+        let lightColor = 0xffffff; 
+        let lightIntensity = 1;
+        let light = new THREE.DirectionalLight(
+            lightColor,
+            lightIntensity,
+        );
+        light.position.set(-1,2,4);
+        scene.add(light);
+    }
+
 
 
     let boxWidth = 1;
@@ -40,15 +51,21 @@ function main(){
         boxDepth
         );
 
-    let boxMaterial = new THREE.MeshBasicMaterial(
-       {color:0xffffff}
+    let boxMaterial = new THREE.MeshPhongMaterial(
+       {color:0xfff888}
        );
 
     let cube = new THREE.Mesh(boxGeometry, boxMaterial);
     scene.add(cube);
-
-    renderer.render(scene, camera);
-
+    function render(timeStamp){
+        timeStamp = timeStamp * 0.001;
+        cube.rotation.x = timeStamp;
+        cube.rotation.y = timeStamp;
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    
+    }
+  requestAnimationFrame(render);
 }
 
 main();
